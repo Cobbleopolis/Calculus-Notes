53cb1ad824cc9c03b0b84355be7f2717c61964a5
Cobbleopolis <cobbleopolis@gmail.com>
2016-10-19 18:04:10 -0400

    bat script now properly makes image assets

--- Mathematica/Chapter2/Section1.wl
+++ Mathematica/Chapter2/Section1.wl
@@ -1,3 +1,2 @@
-SetDirectory[FileNameJoin[{ParentDirectory[ParentDirectory[]], "Chapter2"}]]
 p1 = Plot[Sin[x], {x, 0, 10}]
 Export["test.png", p1]
--- createAssets.bat
+++ createAssets.bat
@@ -1,1 +1,14 @@
-wolfram -script "Mathematica\CreateAssets.wl"
+@echo off
+setlocal enabledelayedexpansion
+cd %~dp0
+if not exist "images" mkdir images
+cd images
+for /R ..\Mathematica %%G in (*.wl) do (
+    cd %~dp0\images
+    set assetPath=%%~pnG
+    set assetPath=!assetPath:*Mathematica\=!
+    if not exist !assetPath! mkdir !assetPath!
+    cd !assetPath!
+    "%PROGRAMFILES%\Wolfram Research\Mathematica\11.0\wolframscript" -f %%G
+)
+endlocal
